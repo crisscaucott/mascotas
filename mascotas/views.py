172913f26526b7a79cedfcc9ac34ab5b629c7f56
Cristian Caucott <criss.acv@gmail.com>
2015-12-02 18:43:03 -0300

    item 2

--- mascotas/views.py
+++ mascotas/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView, ListView
 from django.core.urlresolvers import reverse, reverse_lazy
+from django.db.models.aggregates import Count
 from .models import Mascota, MascotasPerdidas
 
 # Create your views here.
@@ -12,6 +13,20 @@ class MascotaListView(ListView):
 	def get_context_data(self, **kwargs):
 	    context = super(MascotaListView, self).get_context_data(**kwargs)
 	    context['m_perdidas'] = MascotasPerdidas.objects.filter(fecha_encontrado = None).select_related().all()
+
+	    # Item 2, mascotas encontradas que ya hayan sido perdidas
+	    # item2_ids = MascotasPerdidas.objects.values_list('mascota_id', flat = True).annotate(ids_mascotas=Count('mascota_id')).filter(ids_mascotas__gt = 1)
+	    # print item2_ids
+	    context['item2'] = MascotasPerdidas.objects.raw("""select *
+				from mascotas_perdidas as mp, mascota as m
+				where mp.mascota_id in (select mascota_id
+				from mascotas_perdidas
+				group by mascota_id
+				having count(mascota_id) > 1)
+				and (mp.fecha_perdido is not null and mp.fecha_encontrado is null)
+				and m.id = mp.mascota_id
+				""")
+	    
 	    return context;
 
 
